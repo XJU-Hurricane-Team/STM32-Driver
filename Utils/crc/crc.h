20fbe74e588a716516f255961b975e2fd473d0bb
Deadline039 <1315374252@qq.com>
2025-04-30 16:31:55 +0800

    Add CRC8.

--- Utils/crc/crc.h
+++ Utils/crc/crc.h
@@ -1,6 +1,9 @@
-#ifndef __CHECK_H
-#define __CHECK_H
+#ifndef __CRC_H
+#define __CRC_H
 
-uint16_t crc16(uint8_t *start_byte, uint16_t len);
+#include <stdint.h>
 
-#endif // !__CHECK_H
+uint16_t calc_crc16(uint8_t *start_byte, uint16_t len);
+uint8_t calc_crc8(uint8_t *start_byte, uint8_t len);
+
+#endif /* __CRC_H */
